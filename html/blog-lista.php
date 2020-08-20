f3b6170fb4aa5d6f6c7384bd41290ce4c3cce132
Dawid Mazur <dawidm@websitestyle.pl>
2020-08-20 10:47:04 +0200

    header e-sklep correct

--- html/blog-lista.php
+++ html/blog-lista.php
@@ -1,71 +1,62 @@
 <?php include('header.php'); ?>
 
 <div class="container">
-    <div class="blog">
+    <section class="blog">
         <div class="blog__top">
-            <div class="okruszki">
-                <!-- masz je gotowe więc nie piszę -->
-            </div>
 
-            <div class="blog__controls">
-                <label for="blog_categories">
-                    POKAŻ KATEGORIĘ
-                </label>
-                <div class="select_wrapper">
-                    <select name="blog_categories" id="blog_categories">
-                        <option value="all" disabled selected>Wszystkie wpisy</option>
-                        <option value="Kat_1">Kategoria 1</option>
-                        <option value="Kat_2">Kategoria 2</option>
-                    </select>
-                </div>
-                <label for="blog_time">
-                    SORTUJ
-                </label>
-                <div class="select_wrapper">
-                    <select name="blog_time" id="blog_time">
-                        <option value="newest" selected>Od najnowszych</option>
-                        <option value="oldest">Od najstarszych</option>
-                    </select>
-                </div>
-                <label for="blog_show">
-                    POKAŻ
-                </label>
-                <div class="select_wrapper">
-                    <select name="blog_show" id="blog_show">
-                        <option value="16" selected>16</option>
-                        <option value="24">24</option>
-                    </select>
-                </div>
-                <div class="blog__pagination pagination">
-                    <button class="pagination__left">
-                        < </button> <div class="pagination__number">
-                            1
-                </div>
-                <button class="pagination__right">
-                    >
-                </button>
+        </div>
+
+        <div class="blog__center">
+            <div class="posts">
+                <?php for ($i = 1; $i <= 16; $i++) : ?>
+                    <article class="posts__item post">
+                        <div class="post__img">
+                            <img src="<?php asset('blog--menu.png');?>" alt="">
+                        </div>
+
+                        <div class="post__content description">
+                            <div class="description__top">
+                                <div class="description__label description__label--green"> 
+                                    <!-- green , pink , yellow , blue -->
+                                    NOWOŚCI
+                                </div>
+
+                                <div class="description__date">
+                                    24.06.2020
+                                </div>
+                            </div>
+
+                            <div class="description__center">
+                                <div class="text">
+                                    <h4>
+                                        QUIS ELEIFEND PEDE URNA 
+                                        ELIT GRAVIDA JUSTO
+                                    </h4>
+
+                                    <p>
+                                        Cras adipiscing wisi. Curabitur quis nibh. Donec libero dui, eget est a wisi. Praesent justo. Suspendisse sed leo facilisis pharetra. Morbi aliquam euismod nonummy. Class aptent taciti sociosqu ad litora torquent per inceptos hymenaeos. Fusce tellus. Donec consectetuer mollis. Suspendisse semper nisl, blandit lectus. Vivamus dignissim erat at nunc semper erat, fringilla purus sit amet ligula. Sed ultricies quis, feugiat sapien, non dolor...
+                                        bibendum wisi bibendum pede. Vestibulum ante pretium justo. Curabitur ut malesuada id, wisi. Nam eu libero auctor quis, eleifend pede urna elit, gravida justo, at sapien. In hac habitasse platea dictumst. Praesent est ullamcorper ligula eleifend.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="description__bottom">
+                                <a href="" class="link_std">
+                                    więcej
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                <?php endfor; ?>
             </div>
         </div>
-    </div>
+
+        <div class="blog__bottom">
+
+        </div>
+    </section>
 
 </div>
 
-<div class="blog__center">
-    <div class="posts">
-        <?php for ($i = 1; $i <= 16; $i++) : ?>
-            <div class="posts__item post">
-
-            </div>
-        <?php endfor; ?>
-    </div>
-</div>
-
-<div class="blog__bottom">
-    <div class="okruszki">
-        <!-- masz je gotowe więc nie piszę -->
-    </div>
-</div>
-</div>
-</div>
 
 <?php include('footer.php'); ?>
