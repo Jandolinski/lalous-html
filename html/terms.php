89d15ed322dabdf0cbe2be7005bac039e6a8119e
Dawid Mazur <dawidm@websitestyle.pl>
2020-08-20 13:18:09 +0200

    added terms

---
+++ html/terms.php
@@ -0,0 +1,56 @@
+<?php include('header.php'); ?>
+
+<div class="container">
+    <!-- okruszki -->
+    <section class="terms">
+        <header class="terms__header">
+            <h2 class="terms__title">
+                REGULAMIN
+            </h2>
+
+            <div class="terms__short-description">
+                <p>Właścicielem sklepu internetowego lalous.pl (dalej Sklep) jest:
+                </p>
+                <p>DORIANO AISSATOU BEATA MADEJSKA z siedzibą w adres:
+                    Lutza 26, posiadającą numer NIP: 118-006-98-72 REGON: 010850593 (dalej: DORIANO AISSATOU BEATA MADEJSKA).
+                </p>
+            </div>
+        </header>
+
+        <?php for ($i = 1; $i <= 10; $i++) : ?>
+            <div class="terms__subsection ">
+                <div class="subsection">
+                    <div class="subsection__title">
+                        I. DEFINICJE
+                    </div>
+
+                    <div class="subsection__text">
+                        <p>
+                            DORIANO AISSATOU BEATA MADEJSKA z siedzibą w adres: Lutza 26, posiadającą numer NIP: 118-006-98-72 REGON: 010850593 (dalej: DORIANO AISSATOU BEATA MADEJSKA).
+                        </p>
+                        <ol>
+                            <li>Warunkiem korzystania ze Sklepu i składania zamówień na Towar, jest wyrażenie zgody przez Klienta na postanowienia Regulaminu. Korzystanie przez Klienta ze Sklepu oznacza, iż uważnie zapoznał się on z treścią Regulaminu, akceptuje Regulamin i wyraża zgodę na jego postanowienia.
+                            </li>
+                            <li>Zamówienia można składać poprzez stronę internetową Sklepu www.lalous.pl drogą e-mailową pod adresem : info@lalous.pl oraz telefonicznie pod numerem: +48 503507607
+                            </li>
+                            <li>Ceny Towarów znajdujących się w Sklepie podane są w złotych polskich i zawierają VAT.
+                            </li>
+                            <li>Cena podana przy każdym Towarze jest wiążąca w chwili złożenia zamówienia przez Klienta. Sklep zastrzega sobie prawo do zmiany cen Towarów znajdujących się w ofercie, jednak nie dotyczy to Towaru prawidłowo zamówionego przez Klienta.
+                            </li>
+                            <li>Wszystkie Towary znajdujące się w Sklepie są fabrycznie nowe i wolne od wad fizycznych i prawnych.
+                            </li>
+                            <li>Wszelkie rabaty od cen przedstawionych w sklepie regulowane są odrębnym Regulaminem Oferty Rabatowej.
+                            </li>
+                            <li>Towary widoczne na stronie internetowej Sklepu nie są ofertą w rozumieniu kodeksu cywilnego.
+                            </li>
+                            <li>Każde zamówienie złożone przez Klienta wymaga potwierdzenia ze strony Sklepu.
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        <?php endfor; ?>
+    </section>
+</div>
+
+<?php include('footer.php'); ?>
